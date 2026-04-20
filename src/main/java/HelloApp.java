78d51662102e22206264f194f455d223653a2995
shiva2007-web <gnanashiva07@gmail.com>
2026-04-20 13:06:36 +0530

    feat(UC5): optimized with enhanced for-loop (final)

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,17 +2,21 @@ public class HelloApp {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello, World!");
-        } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            // Traditional for loop
-            for (int i = 0; i < args.length; i++) {
-                nameBuilder.append(args[i]);
-                // Add a comma and space except for the last name
-                if (i < args.length - 1) {
-                    nameBuilder.append(", ");
-                }
-            }
-            System.out.println("Hello, " + nameBuilder.toString() + "!");
+            return; // Exit early if no args
         }
+
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        // Enhanced for-loop (For-Each)
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+
+        System.out.println("Hello, " + nameBuilder.toString() + "!");
     }
 }
